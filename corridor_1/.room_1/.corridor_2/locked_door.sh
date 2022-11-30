36b20f51cccc78f524ba9938f1498edd00fb4543
elee3223 <118023226+elee3223@users.noreply.github.com>
2022-11-30 16:25:48 -0500

    Update locked_door.sh

--- corridor_1/.room_1/.corridor_2/locked_door.sh
+++ corridor_1/.room_1/.corridor_2/locked_door.sh
@@ -1,30 +1,34 @@
 #!/bin/bash
 
-echo "locked door. needs special item to unlock.Look through your inventory by putting it as an argument"
+echo "There is a locked door. It seems it needs a special item to unlock. Look through your inventory by running the script again and putting your inventory as an argument"
 echo ""
 
 if [ $# -eq 1 ]
 then
-	if [ $(grep "potion" $1 | wc -l) -gt 1 ]
-		then echo "you picked up too many potions, and your backpack might break. remove one with sed -i ''0,/item/{/item/d;}'' inventory"
-		else
-		for i in $(cat ${1})
-       		 do
-                if [ "$i" != "fire-amulet" ]
-                        then echo "hmm...the door won't open for the $i"
-			sleep 1
-                else
-                        echo "the fire amulet's power unlocked the door! you can now move onto the next area. Move onto .room_2."
-			echo ""
-			break
-                        cp $1 ./.room_2/inventory
-                fi
+	if test -f $1 
+	then
+		if [ $(grep "potion" $1 | wc -l) -gt 1 ]
+			then echo "you picked up too many potions, and your backpack might break. remove one with sed -i ''0,/item/{/item/d;}'' inventory"
+			else
+			for i in $(cat ${1})
+			 do
+			if [ "$i" != "fire-amulet" ]
+				then echo "hmm...the door won't open for the $i"
+				sleep 1
+			else
+				echo "the fire amulet's power unlocked the door! you can now move onto the next area. Move onto .room_2."
+				echo ""
+				break
+				cp $1 ./.room_2/inventory
+			fi
 
-      		  done
+      			  done
+	  echo "$1 doesn't seem to be here... did you bring it from the other room?"
+ 	 echo " to return to the other room cd ../."
 	fi
 elif [ $# -gt 2 ]
 	then 
 		echo "too many arguments. try again." ""
 	else
-		exit 1
+		return
 fi
