f51b158f339913f6c17e463dfc4cc82656065ef8
elee3223 <118023226+elee3223@users.noreply.github.com>
2022-12-02 10:39:22 -0500

    Update moat.sh

--- corridor_1/.room_1/.corridor_2/.room_2/.corridor_3/.room_3/moat.sh
+++ corridor_1/.room_1/.corridor_2/.room_2/.corridor_3/.room_3/moat.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-echo "grass water-moat inventory"
+echo "recall how you took down the wall of fire..."
+sleep 1
+tput setaf 4
+ echo "
+ ./fight.sh fire-wall inventory
+
+\"fire-wall\" is your first argument. This is locking onto your first target with the amulet's first usage.
+\"inventory\" is your second argument. This is how you are accessing the water amulet to use it."
+tput sgr0
 echo""
+echo "Use the same syntax to overcome this obstacle! Use water-moat as your first argument."
 
 opponent_hp=1
 
+if test -f $2
+then
  grass() {
  if [[ "$1" = *"water"* ]]
          then
@@ -12,12 +23,20 @@ opponent_hp=1
                  then
                         ((opponent_hp=0))
                  else
-                         echo "you can't use water"
+                          echo "You don't have a grass-amulet..."
+                         echo "you can't use grass!"
                  fi
          else
-                 echo "theres nothing to use grass against"
+                 echo "theres nothing to use grass against."
  fi
  }
+ else 
+  echo "$2 doesn't seem to be here... did you bring it from the other room?"
+  echo " to return to the other room cd ../."
+  exit 1
+ fi
+ 
+
 
 until [ $opponent_hp -le 0 ]
 do
@@ -28,7 +47,7 @@ do
                 if [ $opponent_hp -le 0 ]
                         then
                                 echo "you invoke the power of the grass amulet. a bridge of vines are summoned"
-                                echo "a path revealed itself     cat .path"
+                                echo "a path revealed itself... cat .path"
                                 echo ""
                                 exit 1
                 fi
