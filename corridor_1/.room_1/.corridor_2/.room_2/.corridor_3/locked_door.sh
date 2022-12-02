bde74bd9da98b3740d7e146413f184310951ba19
elee3223 <118023226+elee3223@users.noreply.github.com>
2022-12-02 11:13:58 -0500

    Update locked_door.sh

--- corridor_1/.room_1/.corridor_2/.room_2/.corridor_3/locked_door.sh
+++ corridor_1/.room_1/.corridor_2/.room_2/.corridor_3/locked_door.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+echo "=============================================="
 echo "locked door. needs special item to unlock.Look through your inventory by putting it as an argument"
 echo
 echo "ex: ./locked_door.sh inventory"
@@ -23,10 +23,14 @@ then
                         then echo "hmm...the door won't open for the $i"
 			sleep 1
                 else
-                        echo "the grass amulet's power unlocked the door! you can now move onto the next area. Move onto .room_3."
+                        echo ""
+			echo "the grass amulet's power unlocked the door! you can now move onto the next area. Move onto .room_3."
 			echo ""
-			break
-                        cp $1 ./.room_3/inventory
+			echo "Remember to take your bag with you! Do this by entering the following command:"
+			echo "mv inventory ./.room_3/inventory"
+			echo ""
+			echo "=============================================="
+			exit 1
                 fi
 
       		  done
