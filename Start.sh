f26130d8d5b0072034646789467683fad571d6af
elee3223 <118023226+elee3223@users.noreply.github.com>
2022-12-02 10:55:15 -0500

    Update Start.sh

--- Start.sh
+++ Start.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
 
-echo "Welcome to the Dungeon! Your adventure is getting start..."
+==============================================
+
+echo "Welcome to the Dungeon! Your adventure is getting started..."
 sleep 2
 
+echo "" 
 echo "Let's make a backpack! Make a file by typing touch inventory"
-
+echo ""
 read -p "" bag
 while [ "$bag" != "touch inventory" ]
 do
         echo "That's not the right syntax, try again!"
+        echo ""
         read -p "" bag
 done
 
 touch inventory
 
 echo "Now let's enter the dungeon!"
+echo ""
 echo "To see your surroundings as your proceed through the dungeon type ls, then cat dungeon"
+echo ""
 echo "We will be moving through by changing directories, or cd."
 echo "HINT: cd corridor_1"
+echo ""
 echo "make sure to bring your backpack with you!"
-echo "HINT: mv inventory corridor_1/inventory"   
+echo "HINT: mv inventory corridor_1/inventory"  
+echo ""
 sleep 1
 printf "\n\nPress any key to continue"
 read -n 1
- fi
 
+
+==============================================
                                
